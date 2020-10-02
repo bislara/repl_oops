4dba645e7186fe0d45af8008bcf64064f74f555b
Jagannath Pandit <35166514+99jagannath@users.noreply.github.com>
2020-10-02 23:09:39 +0530

    adding address variable to constructor

--- overload_constructor.cpp
+++ overload_constructor.cpp
@@ -7,27 +7,39 @@ class Human{
 private:
     int age;
     string name;
+    string address;
 public:
    Human(){
         cout << "default constructor"<<endl;
         age = 0;
         name = "noname";
+       address = "noaddress";
     }
 
     Human(string iname){
         cout << "constructor with name as the parameter"<<endl;
         age = 0;
         name = iname;
+        address = "noaddress";
     }
     Human(int iage){
         cout << "constructor with age as the parameter"<<endl;
         age = iage;
         name = "noname";
+        address = "noaddress";
+    }
+    Human(string add){
+        
+        cout << "constructor with address as the parameter"<<endl;
+        age = 21;
+        name = "noname";
+        address = add;
     }
     Human(string iname,int iage){
         cout << "constructor with age and name as the parameter"<<endl;
         age = iage;
         name = iname;
+        address = "noaddress";
     }
     void display(){
     cout <<name <<endl<<age<<endl;
